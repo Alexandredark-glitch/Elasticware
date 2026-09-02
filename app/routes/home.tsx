7d60c34a62266041711cf835d86f9d77032e9c20
Fortunat Alexandre <fortunathitantsoa@gmail.com>
2026-09-03 01:41:13 +0300

    feat: sentry all completely set up

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "./+types/home";
 import { Link } from "react-router";
-import * as Sentry from '@sentry/react';
+
 
 
 export function meta({}: Route.MetaArgs) {
@@ -76,14 +76,6 @@ export default function LandingPage() {
             >
               Dashboard
             </Link>
-
-            <button
-      onClick={() => {
-        throw new Error('This is your first error!');
-      }}
-    >
-      Break the world
-    </button>
           </nav>
         </div>
        
